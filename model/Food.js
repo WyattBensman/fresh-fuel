45c1117512e651a3af0c472c77211b6ee403bc59
Jese H <pilers2000@hotmail.com>
2023-08-11 09:35:08 -0400

    hooked up the server

---
+++ model/Food.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection'); // Make sure to import your Sequelize instance here
+
+class Food extends Model {}
+
+Food.init(
+{
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        },
+    food_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    ingredients: {
+        type: DataTypes.JSON, // Use JSONB for storing an array of ingredients
+        allowNull: false
+    },
+    instructions: {
+        type: DataTypes.JSON, // Use JSONB for storing an array of instructions
+        allowNull: false
+    },
+    nutrition: {
+        type: DataTypes.JSON, // Use JSONB for storing nutrition information
+        allowNull: false
+    }
+},
+{
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'food',
+}
+);
+
+module.exports = Food;
