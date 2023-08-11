d3434093b11bd67603800a74a1226731792e76d6
Jese H <pilers2000@hotmail.com>
2023-08-11 12:43:41 -0400

    hooked up server to handlebars view

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const { Food } = require('../model/Food');
+const Food = require('../model/Food');
+console.log(Food);
 
 // GET all foods for homepage
 router.get('/', async (req, res) => {
@@ -20,5 +21,23 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+router.post('/', (req, res) => {
+    // Use Sequelize's `create()` method to add a row to the table
+    // Similar to `INSERT INTO` in plain SQL
+    Food.create({
+      food_name: req.body.food_name,
+      ingredients: req.body.ingredients,
+      instructions: req.body.instructions,
+      
+    })
+      .then((newMeal) => {
+        // Send the newly created row as a JSON object
+        res.json(newMeal);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
 
 module.exports = router;
