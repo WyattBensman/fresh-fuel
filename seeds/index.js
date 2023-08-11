50993a0e9968e2b6191f3ab6e88f0a647a3f97f3
JoFel7 <josuefeliciano7@gmail.com>
2023-08-11 13:32:29 -0400

    fixed issuses with index and foodData

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,5 @@
 const sequelize = require("../config/connection");
-const seedFood = require("./paintingData");
+const seedFood = require("./foodData");
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
